b2e2f97aba397c59666ffc469b68fc32d67fb57d
kamciokodzi <kamil.kolodziej@student.put.poznan.pl>
2017-07-12 21:13:10 +0200

    HTML -> JSON for all controllers and views

--- apps/reverso_web/lib/reverso_web/controllers/translation_controller.ex
+++ apps/reverso_web/lib/reverso_web/controllers/translation_controller.ex
@@ -2,58 +2,39 @@ defmodule Reverso.Web.TranslationController do
   use Reverso.Web, :controller
 
   alias Reverso.Projects
-
+  alias Reverso.Projects.Translation
+  
   def index(conn, _params) do
     translations = Projects.list_translations()
-    render(conn, "index.html", translations: translations)
-  end
-
-  def new(conn, _params) do
-    changeset = Projects.change_translation(%Reverso.Projects.Translation{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "index.json", translations: translations)
   end
 
   def create(conn, %{"translation" => translation_params}) do
-    case Projects.create_translation(translation_params) do
-      {:ok, translation} ->
-        conn
-        |> put_flash(:info, "Translation created successfully.")
-        |> redirect(to: translation_path(conn, :show, translation))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+    with {:ok, %Translation{} = translation} <- Projects.create_translation(translation_params) do
+      conn
+        |> put_status(:created)
+        |> put_resp_header("location", translation_path(conn, :show, translation))
+        |> render("show.json", translation: translation)
     end
   end
 
   def show(conn, %{"id" => id}) do
     translation = Projects.get_translation!(id)
-    render(conn, "show.html", translation: translation)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    translation = Projects.get_translation!(id)
-    changeset = Projects.change_translation(translation)
-    render(conn, "edit.html", translation: translation, changeset: changeset)
+    render(conn, "show.json", translation: translation)
   end
 
   def update(conn, %{"id" => id, "translation" => translation_params}) do
     translation = Projects.get_translation!(id)
 
-    case Projects.update_translation(translation, translation_params) do
-      {:ok, translation} ->
-        conn
-        |> put_flash(:info, "Translation updated successfully.")
-        |> redirect(to: translation_path(conn, :show, translation))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", translation: translation, changeset: changeset)
+    with {:ok, %Translation{} = translation} <- Projects.update_translation(translation,translation_params) do
+      render(conn, "show.json", translation: translation)
     end
   end
 
   def delete(conn, %{"id" => id}) do
     translation = Projects.get_translation!(id)
-    {:ok, _translation} = Projects.delete_translation(translation)
-
-    conn
-    |> put_flash(:info, "Translation deleted successfully.")
-    |> redirect(to: translation_path(conn, :index))
+    with {:ok, %Translation{}} = Projects.delete_translation(translation) do
+      send_resp(conn, :no_content, "")
+    end
   end
 end
--- apps/reverso_web/lib/reverso_web/views/project_view.ex
+++ apps/reverso_web/lib/reverso_web/views/project_view.ex
@@ -1,3 +1,20 @@
 defmodule Reverso.Web.ProjectView do
   use Reverso.Web, :view
+  alias Reverso.Web.ProjectView
+
+  def render("index.json", %{projects: projects}) do
+    %{data: render_many(projects, ProjectView, "project.json")}
+  end
+
+  def render("show.json", %{project: project}) do
+    %{data: render_one(project, ProjectView, "project.json")}
+  end
+  
+  def render("project.json", %{project: project}) do
+    %{
+      id: project.id,
+      project: project.platform,
+      title: project.title
+    }
+  end
 end
