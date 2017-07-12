5077c5b292a30c22ce476d439208df5d35eb9845
AppUnite Intern <intern@s6-builds.local>
2017-07-12 09:38:16 +0200

    resolve creating menu using list; vue practice

--- apps/reverso_web/assets/js/components/loggingIn/log-in.vue
+++ apps/reverso_web/assets/js/components/loggingIn/log-in.vue
@@ -1,25 +1,20 @@
 <template>
+
 	<div class="login-box-container" style="margin: 140px auto auto auto; width: 30%; border: 3px solid green; padding: 10px;">
 		<loginBox></loginBox>
 	</div>
 </template>
 
 <script>
-import loginBox from './login-box.vue';
-
-Vue.component('loginBox')
-
-new Vue({
-  el: '#app',
-  render(createElement) {
-    return createElement(loginBox, {})
-  }
-});
+import loginBox from './login-box.vue'
 
 export default {
+  components: {
+    'loginBox': loginBox
+  },
   data() {
     return {
-      messages: ["How are you?"]
+
     }
   }
 }
---
+++ apps/reverso_web/assets/js/components/loggingIn/lost-password.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="lost-pass-box"> 
+    Reverso.co
+    <h2>Lost your password?</h2>
+    Please enter your email address. You will recieve a link to create a new password via e-mail. 
+    <form>
+      Email:<br>
+      <input type="text"><br>
+      <input type="submit" value="Reset Password">
+    </form>
+ </div>
+</template>
+
+<script>
+export default {
+	
+	data() {
+		return {
+
+		} 
+	}
+
+}
+</script>
+
