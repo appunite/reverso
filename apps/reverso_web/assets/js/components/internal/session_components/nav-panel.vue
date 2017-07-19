48f7e7d9c00c050b58857e6ea51b0e488492e72b
Ewa Fengler <ewa.fen@interia.pl>
2017-07-19 16:10:23 +0200

    change logo on nav menu from nav element to normal link

--- apps/reverso_web/assets/js/components/internal/session_components/nav-panel.vue
+++ apps/reverso_web/assets/js/components/internal/session_components/nav-panel.vue
@@ -2,11 +2,11 @@
 	<div id="nav-panel">
 	
 		<el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-		  <el-menu-item index="1"><img src="/images/logo-white.svg"></el-menu-item>
+		  <router-link to="/projects"><img src="/images/logo-white.svg" style="margin: 16px 10px;n"></router-link>
 		  <el-submenu index="2">
 		    <template slot="title">User</template>
 		    <el-menu-item index="2-1"><img src="/images/ic-account.svg">Account</el-menu-item>
-		    <el-menu-item index="2-2" style="color: red;" @click="logout"><img src="/images/ic-logout.svg">Log out</el-menu-item>
+		    <el-menu-item index="2-2" style="color: #fe3c5b;" @click="logout"><img src="/images/ic-logout.svg">Log out</el-menu-item>
 		  </el-submenu>
 		</el-menu>
 
