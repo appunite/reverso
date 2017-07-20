3ccdfdf0c59a9ea0c4e8bf398914c8763b38b281
kamciokodzi <kamil.kolodziej@student.put.poznan.pl>
2017-07-20 12:50:16 +0200

    Seed update

--- apps/reverso/priv/repo/seeds.exs
+++ apps/reverso/priv/repo/seeds.exs
@@ -9,22 +9,72 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# USER 1 activated
 Reverso.Accounts.create_user(
       %{email: "foo@bar.com",
         name: "foobar",
         password: "12345",
         password_confirmation: "12345",
         activation_token: "activate"})
+Reverso.Accounts.activate("activate")
+
+# USER 2 activated
+Reverso.Accounts.create_user(
+      %{email: "mike@schabowy.com",
+        name: "mike",
+        password: "12345",
+        password_confirmation: "12345",
+        activation_token: "activate_mike"})
+Reverso.Accounts.activate("activate_mike")
+
+#USER 3 and 4 not activated 
 Reverso.Accounts.create_user(
   %{email: "example@example.com",
     name: "example",
     password: "12345",
     password_confirmation: "12345",
     activation_token: Ecto.UUID.generate()})
+
 Reverso.Accounts.create_user(
   %{email: "lubie@placki.com",
     name: "lubieplatzky",
     password: "12345",
     password_confirmation: "12345",
     activation_token: Ecto.UUID.generate()})
-Reverso.Accounts.activate("activate")
+
+Reverso.Projects.create_project(
+  %{project_name: "Kiszone Ogorki",
+    basic_language: "English",
+    owner_id: 1}, ["iOS", "Android"])
+
+Reverso.Projects.create_project(
+  %{project_name: "Frytki z Octem",
+    basic_language: "Tasmanian",
+    owner_id: 1}, ["Desktop"])
+
+Reverso.Projects.create_project(
+  %{project_name: "Pomalowane Aluminum",
+    basic_language: "Nihali",
+    owner_id: 1}, ["Android"])
+
+Reverso.Projects.create_project(
+  %{project_name: "Ciastko z Niespodzianka",
+    basic_language: "Burushian",
+    owner_id: 2}, ["Android", "iOS"])
+
+Reverso.Projects.create_project(
+  %{project_name: "Baba z Radomia",
+    basic_language: "Sandawe",
+    owner_id: 2}, ["Desktop", "iOS"])
+
+Reverso.Projects.create_language(%{language_name: "Spanish", project_id: 1 })
+Reverso.Projects.create_language(%{language_name: "German", project_id: 1 })
+Reverso.Projects.create_language(%{language_name: "English", project_id: 2 })
+Reverso.Projects.create_language(%{language_name: "Polish", project_id: 2 })
+Reverso.Projects.create_language(%{language_name: "Spanish", project_id: 3 })
+Reverso.Projects.create_language(%{language_name: "German", project_id: 4 })
+Reverso.Projects.create_language(%{language_name: "English", project_id: 4 })
+Reverso.Projects.create_language(%{language_name: "English", project_id: 5 })
+Reverso.Projects.create_language(%{language_name: "Russian", project_id: 5 })
+
