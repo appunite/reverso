cc780238b21bc0a8dfe213a02401eeedbd5f1ac8
jakubpieta <jakub.pieta@outlook.com>
2017-07-20 10:50:28 +0200

    populating database with seed

--- apps/reverso/priv/repo/seeds.exs
+++ apps/reverso/priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Reverso.Accounts.create_user(
+      %{email: "foo@bar.com",
+        name: "foobar",
+        password: "12345",
+        password_confirmation: "12345",
+        activation_token: "activate"})
+Reverso.Accounts.create_user(
+  %{email: "example@example.com",
+    name: "example",
+    password: "12345",
+    password_confirmation: "12345",
+    activation_token: Ecto.UUID.generate()})
+Reverso.Accounts.create_user(
+  %{email: "lubie@placki.com",
+    name: "lubieplatzky",
+    password: "12345",
+    password_confirmation: "12345",
+    activation_token: Ecto.UUID.generate()})
+Reverso.Accounts.activate("activate")
