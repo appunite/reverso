97485b3db8684bd9be95faa2938b3efdbcdc366b
kamciokodzi <kamil.kolodziej@student.put.poznan.pl>
2017-07-21 13:36:46 +0200

    Sending project after create

--- apps/reverso_web/lib/reverso_web/views/project_view.ex
+++ apps/reverso_web/lib/reverso_web/views/project_view.ex
@@ -6,6 +6,10 @@ defmodule Reverso.Web.ProjectView do
     %{data: render_many(projects, ProjectView, "project.json")}
   end
 
+  def render("show.json", %{project: project}) do
+    %{data: render_one(project,ProjectView, "project.json")}
+  end
+
   def render("project.json", %{project: project}) do
     %{id: project.id,
       project_name: project.project_name,
