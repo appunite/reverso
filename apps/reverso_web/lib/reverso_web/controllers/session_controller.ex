390d9968869910c9993442bf96c395e80784131d
jakubpieta <jakub.pieta@outlook.com>
2017-07-24 11:00:04 +0200

    forgot conn

--- apps/reverso_web/lib/reverso_web/controllers/session_controller.ex
+++ apps/reverso_web/lib/reverso_web/controllers/session_controller.ex
@@ -33,7 +33,7 @@ defmodule Reverso.Web.SessionController do
   end
 
   # THIS WILL BE CHANGED
-  def generate_activation_url(%User{} = user) do
+  def generate_activation_url(conn, %User{} = user) do
     Reverso.Web.Router.token_url(conn, :activate_account, token: user.activation_token)
   end
 
