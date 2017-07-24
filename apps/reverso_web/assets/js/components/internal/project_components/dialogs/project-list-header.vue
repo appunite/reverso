7181df16ad9df7ac9ed9a67230c946d6bc1e87e8
Ewa Fengler <ewa.fen@interia.pl>
2017-07-24 14:05:42 +0200

    project list styling

---
+++ apps/reverso_web/assets/js/components/internal/project_components/dialogs/project-list-header.vue
@@ -0,0 +1,48 @@
+<template>  
+  <span class="summary">
+    <span class="summary-col-1">
+              {{ project_name }}
+            </span>
+
+            <span class="summary-col-2">
+              languages
+              strings
+            </span>
+
+            <span class="summary-col-3">
+              <el-progress :percentage="100" status="success" :show-text="false"></el-progress>
+            </span>
+
+            <span class="summary-col-4">
+                <newContributor></newContributor>
+                <exportSettings></exportSettings>
+                <editProject v-bind:project="project"></editProject>
+            </span>
+          </span>
+</template>
+
+<script>
+import newContributor from './actions/new-contributor.vue'
+import exportSettings from './export-settings.vue'
+import editProject from './actions/edit-project.vue'
+
+export default {
+  name: "projectListHeader",
+
+  props: [
+    'project_name'
+  ],
+
+  components: {
+    'newContributor': newContributor,
+    'exportSettings': exportSettings,
+    'editProject': editProject
+  },
+
+  data () {
+    return {
+    }
+
+  },
+}
+</script>
