11dad55f23316e71f8e71e11b1bf574ec57a14ce
jakubpieta <jakub.pieta@outlook.com>
2017-07-25 13:36:04 +0200

    config

--- apps/reverso/config/config.exs
+++ apps/reverso/config/config.exs
@@ -5,15 +5,4 @@ config :reverso, ecto_repos: [Reverso.Repo]
 config :reverso, Reverso.Mailer,
   adapter: Bamboo.LocalAdapter
 
-# later we will use this config probably:
-#  server: "smtp.gmail.com",
-#  port: 465,
-#  username: "czupakabra96@gmail.com",
-#  password: "internAppunite",
-#  auth: :always,
-#  tls: :if_avaliable,
-#  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"],
-#  ssl: :always,
-#  retries: 1
-
 import_config "#{Mix.env}.exs"
