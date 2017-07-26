688497446950c181c83ad471a3be8eaa8204f158
Ewa Fengler <ewa.fen@interia.pl>
2017-07-26 13:06:09 +0200

    rename colors

--- apps/reverso_web/assets/js/components/internal/translation_components/translation-table.vue
+++ apps/reverso_web/assets/js/components/internal/translation_components/translation-table.vue
@@ -1,109 +1,72 @@
-<template>	
+<template>  
 
+  <div class="translation-table">
+    <div class="top-table-panel">
+      <el-button type="text" class="add_sth_btn">
+        <img src="/images/ic-add.svg">Add new term
 
+      </el-button>
+    </div>
 
-<div class="translation-table">
-<div class="top-table-panel">
-  <el-button type="text" class="add_sth_btn">
-      <img src="/images/ic-add.svg">Add new term
+    <table>
+      <tr class="table-header">
+        <th>reference language</th>
+        <th>translation</th>
+        <th>description</th>
+      </tr>
+    </table>
 
-  </el-button>
-</div>
-<table>
-  <tr class="table-header">
-  <th>reference language</th>
-  <th>translation</th>
-  <th>description</th>
-  </tr>
-  </table>
-  <table class="translation-table__terms">
-    <tr v-for="translation in translations">
-  <th >{{ translation.basic }}</th>
-  <th>{{ translation.translation }}</th>
-  <th>{{ translation.description }}</th>
-  </tr>
-  </table>
-  
-
-</table>
-
-
-	<!-- <el-table
-		:data="translations"
-		border
-		class="translation-table">
-
-		<el-table-column
-			prop="basic"
-			label="reference language">
-
-
-			<template scope="scope">
-				<el-popover trigger="hover" placement="bottom">
-					<p>{{ scope.row.platform_key }}</p>
-					<div slot="reference" class="name-wrapper">
-						{{ scope.row.basic }}
-					</div>
-				</el-popover>
-				kosz
-			</template>
-
-		</el-table-column>
-
-		<el-table-column
-			prop="translation"
-			label="translation">
-		</el-table-column>
-		
-		<el-table-column
-			prop="description"
-			label="developer description">
-		</el-table-column>
-	
-	</el-table> -->
+    <table class="translation-table__terms">
+      <tr v-for="translation in translations">
+        <th >{{ translation.basic }}</th>
+        <th>{{ translation.translation }}</th>
+        <th>{{ translation.description }}</th>
+      </tr>
+    </table>
   </div>
+
 </template>
 
 <script>
 
-export default {
-	name: "translationTable",
-	data () {
-		return {
-			translations: [
-			{
-				basic: "elo",
-				platform_key: "elo_k",
-				description: "ccc",
-				translation: "xdxd",
-				platform_id: 1
-			},
-			{
-				basic: "elo1",
-				platform_key: "elo1_k",
-				description: "xxx",
-				translation: "xdxd1",
-				platform_id: 1
-			},
-			{
-				basic: "elo3",
-				platform_key: "elo3_k",
-				description: "ppp",
-				translation: "xdxd3",
-				platform_id: 2
-			},
-			{
-				basic: "elo4",
-				platform_key: "elo4_k",
-				description: "",
-				translation: "xdxd4",
-				platform_id: 2
-			}
-			]
-		}
-	},
-	methods: {
-	}
-}
-	
+  export default {
+    name: "translationTable",
+    data () {
+      return {
+        translations: [
+        {
+          basic: "elo",
+          platform_key: "elo_k",
+          description: "ccc",
+          translation: "xdxd",
+          platform_id: 1
+        },
+        {
+          basic: "elo1",
+          platform_key: "elo1_k",
+          description: "xxx",
+          translation: "xdxd1",
+          platform_id: 1
+        },
+        {
+          basic: "elo3",
+          platform_key: "elo3_k",
+          description: "ppp",
+          translation: "xdxd3",
+          platform_id: 2
+        },
+        {
+          basic: "elo4",
+          platform_key: "elo4_k",
+          description: "",
+          translation: "xdxd4",
+          platform_id: 2
+        }
+        ]
+      }
+    },
+    methods: {
+    }
+  }
+  
 </script>
