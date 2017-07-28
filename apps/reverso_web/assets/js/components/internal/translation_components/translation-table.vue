4d9ceb621b71c3d5613f39eefded0f739a50730a
Ewa Fengler <ewa.fen@interia.pl>
2017-07-28 12:47:12 +0200

    [frontend] start adding new term

--- apps/reverso_web/assets/js/components/internal/translation_components/translation-table.vue
+++ apps/reverso_web/assets/js/components/internal/translation_components/translation-table.vue
@@ -1,6 +1,6 @@
 <template>  
 
-  <div id="translation-table">
+  <div class="translation-table">
     <div class="top-table-panel">
       <el-button type="text" class="add_sth_btn" @click="addTerm">
         <img src="/images/ic-add.svg">Add new term
@@ -15,28 +15,38 @@
       </tr>
     </table>
 
-    <table id="translation-table__terms">
+    <div class="translation-table__terms">
+    <table>
       <tr v-for="translation in translations">
         <th>
-        <textarea
-        v-model="translation.basic"
-        placeholder="No Reference">
-        </textarea>
+        <el-input
+        type="textarea"
+        :autosize="true"
+        placeholder="No Reference"
+        v-model="translation.basic">
+        </el-input>
         </th>
+
         <th>
-        <textarea
+        <el-input
+        type="textarea"
+        :autosize="true"
         v-model="translation.translation"
         placeholder="No Translation">
-        </textarea>
+        </el-input>
         </th>
+
         <th>
-        <textarea
+        <el-input
+        type="textarea"
+        :autosize="true"
         v-model="translation.description"
         placeholder="No comments added">
-        </textarea>
+        </el-input>
         </th>
       </tr>
     </table>
+    </div>
   </div>
 
 </template>
@@ -78,6 +88,7 @@ export default {
   mounted() {
     translationService.fetchTranslation(this.project_id, this.language_id).then(
       (response) => {
+        console.log(response.data.data);
         this.translations = response.data.data;
       },
 
