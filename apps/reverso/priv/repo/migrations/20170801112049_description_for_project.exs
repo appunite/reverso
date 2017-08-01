4ad9095ecd9170d3d9c19ca0b8336295ba2746bf
kamciokodzi <kamil.kolodziej@student.put.poznan.pl>
2017-08-01 13:30:03 +0200

    fix

--- apps/reverso/priv/repo/migrations/20170801112049_description_for_project.exs
+++ apps/reverso/priv/repo/migrations/20170801112049_description_for_project.exs
@@ -2,7 +2,7 @@ defmodule Reverso.Repo.Migrations.DescriptionForProject do
   use Ecto.Migration
 
   def change do
-    alter table(:projects_projects) do
+    alter table(:projects_translations) do
       add :description, :string
     end 
   end
