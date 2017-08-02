7eddc5f6df3ed8503894ed7d437785d0534fd8c7
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2017-08-02 14:53:05 +0200

    Bump phoenix

--- apps/reverso_web/mix.exs
+++ apps/reverso_web/mix.exs
@@ -26,7 +26,7 @@ defmodule Reverso.Web.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:phoenix_html, "~> 2.6"},
