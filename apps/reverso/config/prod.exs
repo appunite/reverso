a9c0ba71150d5ce362a50d948278d853ec129fd2
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2017-08-02 14:26:48 +0200

    Add repo config for prod

--- apps/reverso/config/prod.exs
+++ apps/reverso/config/prod.exs
@@ -1,1 +1,9 @@
 use Mix.Config
+
+config :reverso, Reverso.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "reverso_dev",
+  hostname: "localhost",
+  pool_size: 10
